8b44b01aa40ba93c404cffa637711aeeff910516
Abraham MB Selebay <xyberkid0@gmail.com>
2022-12-09 15:42:10 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Counties from "./components/Counties"
 import Navbar from "./components/Navbar"
 export default function App() {
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+      <Counties />
+    </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpBDA9shzv6fziHSiegvtQ_edl404YyU4",
+  authDomain: "knowliberia.firebaseapp.com",
+  projectId: "knowliberia",
+  storageBucket: "knowliberia.appspot.com",
+  messagingSenderId: "489536298228",
+  appId: "1:489536298228:web:ce5f9d70a4454a4aea92a1",
+  measurementId: "G-K4DL0K436N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const firestore = getFirestore(app);
