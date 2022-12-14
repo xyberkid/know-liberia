a06ae57b27c31e3263ba9500bfe1d0f025ac3b44
Abraham MB Selebay <xyberkid0@gmail.com>
2022-12-14 16:48:41 +0000

    Counties commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Counties from "./components/Counties"
 import Navbar from "./components/Navbar"
 export default function App() {
+
+  function handleActive(e) {
+    e.preventDefault();
+    console.log("value", e.target)
+    setIsSelected({
+      isActive: true,
+      name: e.target.value
+    })
+  }
   return (
+
     <>
       <Navbar />
       <main>
-        <div class="relative bg-auto">
+        <div class="relative bg-gray">
           <div class="container mx-auto">
             <div class="-mx-4 flex flex-wrap">
               <div class="w-full px-4 lg:w-5/12">
@@ -14,12 +24,15 @@ export default function App() {
                     class="text-dark mb-3 text-4xl font-bold leading-snug sm:text-[42px] lg:text-[40px] xl:text-[42px]"
                   >
                     Welcome <br />
-                    To Know
-                    Liberia.
+                    To Know Liberia.
                   </h1>
                   <p class="text-body-color mb-8 max-w-[480px] text-base">
-                    Know Liberia is a being built specifically for Liberian and non-liberian
-                    to have access to reliable information concerning Liberia and it people.
+                    Liberia officially the Republic of Liberia, is a country in West Africa.
+                    It is bordered by Sierra Leone on the west, Guinea on the north and Ivory
+                    Coast on the east. Liberia covers an area of 111,369 km2 (43,000 sq mi)
+                    and is home to about 5.4 million people. English is the official language,
+                    while over 30 indigenous languages are spoken within the country.
+
                   </p>
                   <ul class="flex flex-wrap items-center">
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
@@ -30,10 +43,28 @@ export default function App() {
                       </div>
                       <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                          <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">County</a>
+                          <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Counties</label>
+                          <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Choose a county</option>
+                            <option value="bomi">Bomi</option>
+                            <option >Bong</option>
+                            <option >Gbarpolu</option>
+                            <option >Grand Bassa</option>
+                            <option >Grand Cape Mount</option>
+                            <option>Grand Gedeh</option>
+                            <option>Grand Kru</option>
+                            <option>Lofa</option>
+                            <option>Margibi</option>
+                            <option>Marry Land</option>
+                            <option>Montserrado</option>
+                            <option>Nimba</option>
+                            <option>Rivercess</option>
+                            <option>River Gee</option>
+                            <option>Sinoe</option>
+                          </select>
                         </li>
                         <li>
-                          <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+
                         </li>
                       </ul>
                     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Navbar() {
     return (
 
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-blue-900">
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
             <div class="container flex flex-wrap items-center justify-between mx-auto">
                 <a href="/" class="flex items-center">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Flag_of_Liberia.svg/800px-Flag_of_Liberia.svg.png?20210909153121" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
